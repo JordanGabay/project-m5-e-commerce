9f4d3162aabad7f2192fc51c26878d9fd09e8c02
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-08-11 17:52:04 -0400

    Added GoogleOAuth to Login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,35 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import { GoogleLogin } from "react-google-login";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [url, setUrl] = useState("");
+  const [googleId, setGoogleId] = useState("");
 
-export default Login;
+  const responseGoogle = (res) => {
+    const { name, email, imageUrl, googleId } = res.profileObj;
+    setName(name);
+    setEmail(email);
+    setGoogleId(googleId);
+    setUrl(imageUrl);
+  };
+
+  return (
+    <div className="App">
+      <GoogleLogin
+        clientId={process.env.CLIENT_ID} // How to insert API key hidden in .env?
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+
+      <h1>Login with Google</h1>
+      <h2>Welcome: {name}</h2>
+      <h2>Email: {email}</h2>
+      <h3>Google Id: {googleId}</h3>
+      <img src={url} />
+    </div>
+  );
+}

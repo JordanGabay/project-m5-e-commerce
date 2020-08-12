b186ae8badbec9c38f3c63fc976ab460cf381a6e
ethanbz <ethanzhivotovsky@gmail.com>
2020-08-12 13:39:41 -0400

    bug fix

--- client/src/components/ProductFeed.js
+++ client/src/components/ProductFeed.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link, useRouteMatch, useHistory, useLocation } from 'react-router-dom'
 import {useDispatch} from 'react-redux';
